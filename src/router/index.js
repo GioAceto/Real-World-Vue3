e879fcb7dfb93e38b726a4ac49cc7accc6de40bb
Giovanni Aceto <55949483+GioAceto@users.noreply.github.com>
2023-02-08 09:06:21 -0500

    add 404 component and route

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import EventListView from '../views/EventListView.vue'
 import EventDetailsView from '../views/EventDetailsView.vue'
 import AboutView from '../views/AboutView.vue'
+import NotFound from '@/components/NotFound.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -21,6 +22,10 @@ const router = createRouter({
       path: '/about',
       name: 'about',
       component: AboutView,
+    },
+    {
+      path: '/:pathMatch(.*)',
+      component: NotFound,
     },
   ],
 })
